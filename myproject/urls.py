ec916e1f9e2e85508de0d42c1333f5d54bd0e4c0
SSakashita <sakashita1969@gmail.com>
2020-10-31 22:59:15 +0900

    Update the first page.

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('', include('firstpage.urls')),
@@ -12,5 +16,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
